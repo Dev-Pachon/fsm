b6b600544da381e38966d7ca9081988bf9564b83
Dev.Pachon <54422629+Dev-Pachon@users.noreply.github.com>
2022-03-11 00:53:45 -0500

    Add Taildwind css, and started to create ui

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,3 +1,13 @@
 export default function HomePage(){
-	return(<div><h1>HIII</h1></div>)
+	return(
+		<div className="flex flex-column">
+			<div className="flex flex-row container mx-auto">
+				<input type="text" className="w-full border"/>
+				<input type="text" className="w-full border"/>
+				<input type="text" className="w-full border"/>
+				<input type="text" className="w-full border"/>
+				<input type="text" className="w-full border"/>
+			</div>
+		</div>
+	)
 }
